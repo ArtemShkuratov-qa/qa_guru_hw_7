9228f17bce982dddc17e263d3fa791f1b474d36a
ArtemShkuratov-qa <artem.shkuratov001@gmail.com>
2025-04-12 14:23:22 +0300

    Init commit

---
+++ conftest.py
@@ -0,0 +1,21 @@
+import os
+from zipfile import ZipFile
+
+import pytest
+
+CURRENT_FILE = os.path.abspath(__file__)
+CURRENT_DIR = os.path.dirname(CURRENT_FILE)
+TMP_DIR = os.path.join(CURRENT_DIR, 'tmp')
+file_dir = os.listdir(TMP_DIR)
+
+@pytest.fixture()
+def create_folder():
+    if not os.path.exists('resources'):
+        os.mkdir('resources')
+
+@pytest.fixture()
+def create_zip():
+    with ZipFile('resources/companies.zip', 'w') as my_zip:
+        for file in file_dir:
+            add_file = os.path.join('tmp', file)
+            my_zip.write(add_file)
