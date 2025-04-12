c0dfde226ae61791cd8871ac88ba40559bb88691
Artem Shkuratov <artem_shkuratov@bk.ru>
2025-04-12 16:47:00 +0300

    fix test

--- conftest.py
+++ conftest.py
@@ -8,12 +8,12 @@ CURRENT_DIR = os.path.dirname(CURRENT_FILE)
 TMP_DIR = os.path.join(CURRENT_DIR, 'tmp')
 file_dir = os.listdir(TMP_DIR)
 
-@pytest.fixture()
+@pytest.fixture(scope='function', autouse=True)
 def create_folder():
     if not os.path.exists('resources'):
         os.mkdir('resources')
 
-@pytest.fixture()
+@pytest.fixture(scope='function', autouse=True)
 def create_zip():
     with ZipFile('resources/companies.zip', 'w') as my_zip:
         for file in file_dir:
--- test_main.py
+++ test_main.py
@@ -1,15 +1,21 @@
 import os
-from zipfile import ZipFile
+import csv
+import zipfile
 
 CURRENT_FILE = os.path.abspath(__file__)
 CURRENT_DIR = os.path.dirname(CURRENT_FILE)
 TMP_DIR = os.path.join(CURRENT_DIR, 'tmp')
+RES_DIR = os.path.join(CURRENT_DIR, 'resources')
 file_dir = os.listdir(TMP_DIR)
 
-if not os.path.exists('resources'):
-    os.mkdir('resources')
+def test_csv():
+    with zipfile.ZipFile(os.path.join(RES_DIR, 'companies.zip')) as zf:
+        with zf.open('tmp/import_company_csv.csv') as csv_file:
+            content = csv_file.read().decode('utf-8-sig')
+            csvreader = list(csv.reader(content.split()))  # читаем содержимое файла и преобразуем его в список
+            second_row = csvreader[1]  # получаем вторую строку
+            print(csvreader)
+            print(second_row)
+            #assert second_row[0] == 'OU001'  # проверка значения элемента в первом столбце второй строки
+            #assert second_row[2] == 'ЗАО"Сервис"'
 
-with ZipFile('resources/companies.zip', 'w') as my_zip:
-    for file in file_dir:
-        add_file = os.path.join('tmp', file)
-        my_zip.write(add_file)
