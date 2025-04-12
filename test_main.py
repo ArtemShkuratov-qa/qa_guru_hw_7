611f0505b0bc513c06be86fc265f258f422d5de6
ArtemShkuratov-qa <artem.shkuratov001@gmail.com>
2025-04-12 14:24:34 +0300

    add files

---
+++ test_main.py
@@ -0,0 +1,15 @@
+import os
+from zipfile import ZipFile
+
+CURRENT_FILE = os.path.abspath(__file__)
+CURRENT_DIR = os.path.dirname(CURRENT_FILE)
+TMP_DIR = os.path.join(CURRENT_DIR, 'tmp')
+file_dir = os.listdir(TMP_DIR)
+
+if not os.path.exists('resources'):
+    os.mkdir('resources')
+
+with ZipFile('resources/companies.zip', 'w') as my_zip:
+    for file in file_dir:
+        add_file = os.path.join('tmp', file)
+        my_zip.write(add_file)
