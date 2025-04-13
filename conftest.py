ac7b2376bf12032e09c85257af1220feaa431c58
ArtemShkuratov-qa <artem.shkuratov001@gmail.com>
2025-04-13 12:44:11 +0300

    fix tests

--- conftest.py
+++ conftest.py
@@ -1,20 +1,20 @@
 import os
 from zipfile import ZipFile
-
 import pytest
 
 CURRENT_FILE = os.path.abspath(__file__)
 CURRENT_DIR = os.path.dirname(CURRENT_FILE)
 TMP_DIR = os.path.join(CURRENT_DIR, 'tmp')
+RES_DIR = os.path.join(CURRENT_DIR, 'resources')
 file_dir = os.listdir(TMP_DIR)
 
+
+# Создание архива
 @pytest.fixture(scope='function', autouse=True)
-def create_folder():
+def create_archive():
     if not os.path.exists('resources'):
         os.mkdir('resources')
 
-@pytest.fixture(scope='function', autouse=True)
-def create_zip():
     with ZipFile('resources/companies.zip', 'w') as my_zip:
         for file in file_dir:
             add_file = os.path.join('tmp', file)
--- test_main.py
+++ test_main.py
@@ -1,21 +1,46 @@
 import os
 import csv
 import zipfile
+from pypdf import PdfReader
+from openpyxl import load_workbook
+from conftest import RES_DIR
 
-CURRENT_FILE = os.path.abspath(__file__)
-CURRENT_DIR = os.path.dirname(CURRENT_FILE)
-TMP_DIR = os.path.join(CURRENT_DIR, 'tmp')
-RES_DIR = os.path.join(CURRENT_DIR, 'resources')
-file_dir = os.listdir(TMP_DIR)
 
 def test_csv():
     with zipfile.ZipFile(os.path.join(RES_DIR, 'companies.zip')) as zf:
         with zf.open('tmp/import_company_csv.csv') as csv_file:
             content = csv_file.read().decode('utf-8-sig')
-            csvreader = list(csv.reader(content.split()))  # читаем содержимое файла и преобразуем его в список
-            second_row = csvreader[1]  # получаем вторую строку
-            print(csvreader)
-            print(second_row)
-            #assert second_row[0] == 'OU001'  # проверка значения элемента в первом столбце второй строки
-            #assert second_row[2] == 'ЗАО"Сервис"'
+            csvreader = list(csv.reader(content.splitlines()))
+            second_row = csvreader[1]
+
+            assert second_row[0] == 'OU001'
+            assert second_row[2] == 'ЗАО Сервис'
+            print('Тест пройден')
+
+
+def test_pdf():
+    with zipfile.ZipFile(os.path.join(RES_DIR, 'companies.zip')) as zf:
+        with zf.open('tmp/import_company_pdf.pdf') as pdf_file:
+            reader = PdfReader(pdf_file)
+            number_of_pages = len(reader.pages)
+            page = reader.pages[0]
+            text = page.extract_text()
+            row = list(text.splitlines())
+
+            assert  number_of_pages == 2
+            assert row[0] == 'Внешний идентификатор для импорта Вышестоящий отдел'
+            assert row[4] == 'OU004'
+            print('Тест пройден')
+
+
+def test_xlsx():
+    with zipfile.ZipFile(os.path.join(RES_DIR, 'companies.zip')) as zf:
+        with zf.open('tmp/import_company_xlsx.xlsx') as xlsx_file:
+            workbook = load_workbook(xlsx_file)
+            sheet = workbook.active
+
+            assert sheet.cell(row=6, column=3). value == 'Филиал 1'
+            assert sheet.cell(row=6, column=4).value == 'Челябинск'
+            print('Тест пройден')
+
 
